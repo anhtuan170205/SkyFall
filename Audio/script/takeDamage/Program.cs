04aa4916687954896814912c29a32a46faffd3b6
Rainnie <nguyensmaison@gmail.com>
2025-03-23 20:46:25 +0700

    Create Program.cs

---
+++ Audio/script/takeDamage/Program.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private AudioClip hitSound;  // Âm thanh khi trúng đạn
+    private AudioSource audioSource;
+    private int health = 3;  // Máu của kẻ địch
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (hitSound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(hitSound);
+        }
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
